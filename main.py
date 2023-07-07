a7b62b8e4acba7afa2b954f2f92ee708b2b6c8ae
ryanminjaeshin <ryanminjaeshin@gmail.com>
2023-07-07 14:34:42 +0900

    tood

---
+++ main.py
@@ -0,0 +1,18 @@
+todos = []
+
+while True:
+    user_action = input("Type add, show, edit or exit:")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item}"
+                print(row)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+
